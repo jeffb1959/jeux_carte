b783edfc238e14b73f94be0c6929f2da7d3be7c2
jeffb1959 <148492025+jeffb1959@users.noreply.github.com>
2025-11-15 09:32:59 -0500

    Delete dame_de_pique_config.js

--- dame_de_pique_config.js
+++
@@ -1,8 +0,0 @@
-window.firebaseConfig = {
-      apiKey: "AIzaSyDLSnoOCdHXl-5sKaJ55hhFbv-quS156kg",
-      authDomain: "soireecartev2.firebaseapp.com",
-      projectId: "soireecartev2",
-      storageBucket: "soireecartev2.firebasestorage.app",
-      messagingSenderId: "784781686361",
-      appId: "1:784781686361:web:033693cb22fb1a55af2348"
-    };
